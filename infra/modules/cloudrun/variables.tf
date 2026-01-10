d8828bd32153fdaa773a2103b724a9a342d23229
akp2609 <amanpandey1910@gmail.com>
2026-01-10 14:18:47 +0000

    cloud run auto container build step added

--- infra/modules/cloudrun/variables.tf
+++ infra/modules/cloudrun/variables.tf
@@ -1,11 +1,19 @@
 variable "project_id" {}
-variable "service_name" {}
 variable "region" {
   default = "us-central1"
 }
-variable "image" {}
 variable "service_account" {}
 variable "env_vars" {
   type = map(string)
   default = {}
 }
+variable "image" {
+  type = string
+}
+variable "service_name" {
+  type = string
+}
+variable "project_id" {
+  type = string
+}
+
