d7ac6e137f917f4e563808dbaa6bb64a67fe4a27
akp2609 <amanpandey1910@gmail.com>
2026-02-23 23:18:34 +0000

    metrics insights update

--- services/analysis-engine/internal/repository/metrics_repo.go
+++ services/analysis-engine/internal/repository/metrics_repo.go
@@ -3,18 +3,32 @@ package repository
 import (
     "context"
     "time"
+	"database/sql"
 
     "github.com/amanpandey1910/cloud-project-analyzer/analysis-engine/internal/models"
 )
 
 
-func (r *Repository) GetMetrics(ctx context.Context, projectID string, metricType string, since time.Time) ([]models.ProjectMetric, error) {
-    rows, err := r.db.QueryContext(ctx, `
-        SELECT project_id, service_name, metric_type, window_start, window_end, value, created_at, collected_at
-        FROM project_metrics
-        WHERE project_id = $1 AND metric_type = $2 AND window_start >= $3
-        ORDER BY window_start DESC
-    `, projectID, metricType, since)
+
+func (r *Repository) GetMetrics(ctx context.Context, projectID, metricType string, since time.Time) ([]models.ProjectMetric, error) {
+    var rows *sql.Rows
+    var err error
+
+    if metricType == "" {
+        rows, err = r.db.QueryContext(ctx, `
+            SELECT project_id, service_name, metric_type, window_start, window_end, value, created_at, collected_at
+            FROM project_metrics
+            WHERE project_id = $1 AND window_start >= $2
+            ORDER BY window_start DESC
+        `, projectID, since)
+    } else {
+        rows, err = r.db.QueryContext(ctx, `
+            SELECT project_id, service_name, metric_type, window_start, window_end, value, created_at, collected_at
+            FROM project_metrics
+            WHERE project_id = $1 AND metric_type = $2 AND window_start >= $3
+            ORDER BY window_start DESC
+        `, projectID, metricType, since)
+    }
     if err != nil {
         return nil, err
     }
